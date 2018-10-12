bffb62eca2fe9c36e47f0f6983eaf0786cf67cc6
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-10-12 17:53:51 +0200

    create Skeleton Methods

--- src/de/hft/wiest_wolf/ShellSort.java
+++ src/de/hft/wiest_wolf/ShellSort.java
@@ -2,8 +2,42 @@ package de.hft.wiest_wolf;
 
 public class ShellSort
 {
+    public static long startTime;
+    public static long stopTime;
+
     public static void main(String[] args)
     {
         // TODO code application logic here
     }
+
+    public static long erikSort(int[] input)
+    {
+        return stopTime - startTime;
+    }
+
+    public static long lukasSort(int[] input)
+    {
+        return stopTime - startTime;
+    }
+
+    public static int[] bubbleSort(int[] input)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int[] generate(int size)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int getMax(int[] input)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int getMin(int[] input)
+    {
+        throw new UnsupportedOperationException();
+    }
+
 }
