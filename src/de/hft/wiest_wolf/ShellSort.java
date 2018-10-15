e1e9221ee52b46155555e321b39057e694334293
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-10-15 10:41:51 +0200

    restructure main and add benchmark method

--- src/de/hft/wiest_wolf/ShellSort.java
+++ src/de/hft/wiest_wolf/ShellSort.java
@@ -1,6 +1,9 @@
 package de.hft.wiest_wolf;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -11,14 +14,57 @@ public class ShellSort
     public static long startTime;
     public static long stopTime;
 
+    enum Algo
+    {
+        ARRAYSSORT("Arrays.sort")
+        {
+            @Override
+            public void sort(int[] values)
+            {
+                Arrays.sort(values);
+            }
+        },
+        LUKASSORT("Lukas-Sort")
+        {
+            @Override
+            public void sort(int[] values)
+            {
+                lukasSort(values);
+            }
+        },
+        BUBBLESORT("Bubblesort")
+        {
+            @Override
+            public void sort(int[] values)
+            {
+                bubbleSort(values);
+            }
+        };
+
+        String name;
+
+        Algo(String name)
+        {
+            this.name = name;
+        }
+
+        public abstract void sort(int[] values);
+
+        @Override
+        public String toString()
+        {
+            return name;
+        }
+    }
+
     public static void main(String[] args)
     {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm");
 //        DecimalFormat tf = new DecimalFormat("0000.0000 seconds");
-        File resultFile = new File("DSA_WW_SortBench_" + df.format(new Date()));
+        String resultFile = "DSA_WW_SortBench_" + df.format(new Date());
         StringBuilder buf = new StringBuilder();
 
-        final int TESTSIZE = 200000;
+        final int TESTSIZE = 10000;
         int[] generated = generate(TESTSIZE);
 
 //        for (int i: generated)
@@ -31,64 +77,56 @@ public class ShellSort
 //            System.out.println(i);
 //        System.exit(0);
 
+        benchmark(TESTSIZE);
+        System.exit(0);
+    }
+
+    public static void benchmark(int size)
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm");
+        String resultFile = "DSA_WW_SortBench_" + df.format(new Date());
+        long[] runs;
+        int[] generated = generate(size);
         int[] range;
 
-        buf.append("array size: \t ");
-        buf.append(TESTSIZE);
-        buf.append("\n\n");
-
-        buf.append("algorithm: \t Arrays.sort\n");
-        buf.append("needed Time:\n");
-        for (int i=0; i < 5; i++)
+        for (Algo alg: Algo.values())
         {
-            range = Arrays.copyOf(generated, generated.length);
-            startTime = System.nanoTime();
-            Arrays.sort(range);
-            stopTime = System.nanoTime();
-            buf.append("run ");
-            buf.append(i+1);
-            buf.append(": \t\t ");
-            buf.append(String.format("%f seconds", ((double) (stopTime - startTime) / 1000000000d)));
-            buf.append("\n");
-        }
-        buf.append("\n\n\n");
-        System.out.println(buf);
+            System.out.println("algorithm:\t" + alg);
+            System.out.println("array-size:\t" + size);
+            System.out.println("needed Time:");
 
-        buf = new StringBuilder();
-        buf.append("algorithm: \t lukasSort\n");
-        buf.append("needed Time:\n");
-        for (int i=0; i < 5; i++)
-        {
-            range = Arrays.copyOf(generated, generated.length);
-            startTime = System.nanoTime();
-            lukasSort(range);
-            stopTime = System.nanoTime();
-            buf.append("run ");
-            buf.append(i+1);
-            buf.append(": \t\t ");
-            buf.append(String.format("%f seconds", ((double) (stopTime - startTime) / 1000000000d)));
-            buf.append("\n");
-        }
-        buf.append("\n\n\n");
-        System.out.println(buf);
+            runs = new long[10];
+            long avg = 0;
+            for (int i=0; i < runs.length; i += 2)
+            {
+                range = Arrays.copyOf(generated, generated.length);
+                runs[i] = System.nanoTime();
+                alg.sort(range);
+                runs[i+1] = System.nanoTime();
+                avg += runs[i+1];
+                avg -= runs[i];
+                System.out.println("run " + (i/2) + ":\t\t" + ((runs[i+1] - runs[i]) / 1000000000d) + "\tseconds");
+            }
+            System.out.println("\n\n");
+            avg /= (runs.length / 2);
 
-        buf = new StringBuilder();
-        buf.append("algorithm: \t bubblesort\n");
-        buf.append("needed Time:\n");
-        for (int i=0; i < 5; i++)
-        {
-            range = Arrays.copyOf(generated, generated.length);
-            startTime = System.nanoTime();
-            bubbleSort(range);
-            stopTime = System.nanoTime();
-            buf.append("run ");
-            buf.append(i+1);
-            buf.append(": \t\t ");
-            buf.append(String.format("%f seconds", ((double) (stopTime - startTime) / 1000000000d)));
-            buf.append("\n");
+            try
+            {
+                File file = new File(resultFile + "_" + alg);
+                if (!file.exists())
+                {
+                    file.createNewFile();
+                }
+                PrintWriter pw = new PrintWriter(new FileOutputStream(file, true), true);
+                pw.println(size + "\t" + (avg / 1000000000d));
+                pw.close();
+            }
+            catch (IOException e)
+            {
+                System.err.println("Couldn't save runtime for algorithm: " + alg + " for size: " + size);
+                e.printStackTrace();
+            }
         }
-        buf.append("\n\n\n");
-        System.out.println(buf);
     }
 
     public static long erikSort(int[] input)
